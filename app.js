3b4e2ad28cd465763e124e1f2d471da8a2d5b016
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-09-14 17:54:25 -0300

    Added the first test.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     path = require('path'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    http = require('http');
 
 var routes = require('./routes/index'),
     users = require('./routes/users'),
@@ -34,7 +35,7 @@ if (app.get('env') === 'development') {
     res.render('error', {
       message: err.message,
       error: err
-    })
+    });
   });
 }
 
@@ -48,5 +49,25 @@ app.use(function (err, req, res, next) {
     error: {}
   });
 });
+
+var server = http.createServer(app);
+var boot = function () {
+  server.listen(app.get('port'), function () {
+    console.info('Express server listening on port ' + app.get('port'));
+  });
+};
+
+var shutdown = function () {
+  server.close();
+};
+
+if (require.main === module) {
+  boot();
+} else {
+  console.info('Running app as a module.');
+  exports.boot = boot;
+  exports.shutdown = shutdown;
+  exports.port = app.get('port');
+}
 
 module.exports = app;
---
+++ tests/index.js
@@ -0,0 +1,24 @@
+var boot = require('../app').boot,
+    shutdown = require('../app').shutdown,
+    port = require('../app').port,
+    superagent = require('superagent'),
+    expect = require('expect.js');
+
+describe('Server', function () {
+  before(function () {
+    boot();
+  });
+  describe('Homepage', function () {
+    it('should respond to GET', function (done) {
+      superagent
+        .get('http://localhost:' + port)
+        .end(function (res) {
+          expect(res.status).to.equal(200);
+          done();
+        });
+    });
+  });
+  after(function () {
+    shutdown();
+  });
+});

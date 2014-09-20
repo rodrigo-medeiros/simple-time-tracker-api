6fc4d4082c2e0dd806d220634f587c116505b792
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-09-20 17:49:16 -0300

    Added moment as a dependency.

--- tests/index.js
+++ tests/index.js
@@ -2,13 +2,16 @@ var boot = require('../app').boot,
     shutdown = require('../app').shutdown,
     port = require('../app').port,
     superagent = require('superagent'),
-    expect = require('expect.js');
+    expect = require('expect.js'),
+    moment = require('moment');
 
 describe('Server', function () {
   before(function () {
     boot();
   });
   describe('Homepage', function () {
+    before(function () {
+
     it('should respond to GET', function (done) {
       superagent
         .get('http://localhost:' + port)

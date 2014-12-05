7719a6b1b67fe6b050f5b48a552d1eee1900f1d1
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-12-04 23:45:48 -0200

    Fixed the _id problem in the returned json.

--- models/user.js
+++ models/user.js
@@ -62,4 +62,12 @@ userSchema.static({
   }
 });
 
+userSchema.set('toJSON', {
+  transform: function (doc, ret, options) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret._v;
+  }
+});
+
 module.exports = mongoose.model('User', userSchema);

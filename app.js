11a076bda82c7cd91a77aa5e43bcf8d934472958
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-12-16 23:36:23 -0200

    Enabled CORS.

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var express = require('express'),
     db = mongoose.connect(dbUrl, {safe: true});
 
 var app = express();
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+app.use(allowCrossDomain);
 
 app.use(function (req, res, next) {
   if (!models.User || !models.Task || !models.Worklog) return next(new Error('There are no models.'));

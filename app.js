41fb1ae4e6e06aeaa7b13d87fc92325078d7b024
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2015-01-06 14:37:08 -0200

    JSHinting.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-}
+};
 
 app.use(allowCrossDomain);
 

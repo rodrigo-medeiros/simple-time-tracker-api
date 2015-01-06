cf15c5f7c49e3fca3cf36035efbcefefb69d6f03
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2015-01-06 13:38:16 -0200

    Removed worklog references.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,2 @@
 exports.user = require('./user');
 exports.task = require('./task');
-exports.worklog = require('./worklog');
-
-exports.index = function (req, res, next) {
-  req.models.Task.find({}, null, {sort: {id: -1}}, function (error, tasks) {
-    if (error) return next(error);
-    res.json(JSON.stringify(tasks));
-  });
-};
